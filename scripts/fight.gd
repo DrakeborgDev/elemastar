b86fd0b7281411536d2b52723f26f062553a0e93
DrakeborgDev <splattercrafter57@gmail.com>
2025-05-21 16:17:50 -0400

    Player can now select from equipped attacks for damage

--- scripts/fight.gd
+++ scripts/fight.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var enemyMaxHealth
+var equippedAttacks = []
 
 func _ready() -> void:
 	print("Region: ", str(GlobalValues.activeRegion), ", Element: ", str(GlobalValues.affinityElement), ", Versing: ", str(GlobalValues.opponent), ", Resetting Affinity Modifier")
@@ -8,37 +9,13 @@ func _ready() -> void:
 	$PlayerDetails/XP.text = "XP: "+str(int(GlobalValues.xp))
 	$PlayerDetails/Health.text = "Health: "+str(int(GlobalValues.playerCurentHealth)) + "/" + str(int(GlobalValues.playerMaxHealth))
 	$EnemyDetails/Affinity.text = GlobalValues.affinityElement + " at strenght " + str(GlobalValues.affinityModifier)
-	var enemyDir = "res://JSON/Enemies/" + GlobalValues.opponent +".json"
-	var curFile_as_text = FileAccess.get_file_as_string(enemyDir)
-	var json = JSON.new()
-	var curFile_as_dict = json.parse(curFile_as_text)
-	if curFile_as_dict == OK:
-		var data = json.data
-		enemyMaxHealth = int(data.health)
-		$EnemyDetails/Name.text = data.name
-		$EnemyDetails/Health.text = "Health: " + str(int(data.health)) + "/" + str(enemyMaxHealth)
-		GlobalValues.opponent = [data.health] #0
-		GlobalValues.opponent.append(data.attacks)#1
-		GlobalValues.opponent.append(data.weaknesses)#2
-		GlobalValues.opponent.append(data.resistances)#3
-		GlobalValues.opponent.append(data.rewards)#4
-	else:
-		print("JSON Parse Error: ", json.get_error_message(), " in ", enemyDir, " at line ", json.get_error_line())
+	_load_enemy()
+	_load_player_attacks()
 
 func _enemy_attack():
 	var attacks = GlobalValues.opponent[1]
 	var activeAttack = attacks[randi_range(0, attacks.size()-1)]
-	match activeAttack.type:
-		GlobalValues.affinityElement:
-			print("matches")
-			GlobalValues.playerCurentHealth -= (activeAttack.damage + GlobalValues.affinityModifier)
-		"Force":
-			print("no modification")
-			GlobalValues.playerCurentHealth -= activeAttack.damage
-		_:
-			print("did not match")
-			if (activeAttack.damage - GlobalValues.affinityModifier) >= 0: 
-				GlobalValues.playerCurentHealth -= (activeAttack.damage - GlobalValues.affinityModifier)
+	GlobalValues.playerCurentHealth -= _attack_affinity(activeAttack.type, activeAttack.damage)
 	if GlobalValues.playerCurentHealth <= 0:
 		print("mission failed, you'll get them next time")
 		GlobalValues.playerCurentHealth = GlobalValues.playerMaxHealth
@@ -58,6 +35,7 @@ func _on_turn_advanced():
 		print("You Win!")
 		GlobalValues.xp += GlobalValues.opponent[4].xp
 		GlobalValues.bittergems += GlobalValues.opponent[4].bittergem
+		GlobalValues.playerCurentHealth += int((GlobalValues.playerMaxHealth - GlobalValues.playerCurentHealth)/2)
 		GlobalValues.affinityElement = null
 		GlobalValues.opponent = null
 		get_tree().change_scene_to_file("res://menus/hub_menu.tscn")
@@ -70,7 +48,60 @@ func _on_skip_pressed() -> void:
 	print(GlobalValues.opponent)
 	_on_turn_advanced()
 
+func _attack_affinity(type: String, baseDamage: int) -> int:
+	match type:
+		GlobalValues.affinityElement:
+			print("boosted")
+			return  baseDamage + GlobalValues.affinityModifier
+		"Force":
+			print("unaffected")
+			return baseDamage
+		_:
+			print("diminished")
+			var total = baseDamage - GlobalValues.affinityModifier
+			if total <= 0:
+				return 0
+			else:
+				return total
 
 func _on_fight_pressed() -> void:
-	GlobalValues.opponent[0] -= (1 + GlobalValues.affinityModifier)
+	print(str(int(equippedAttacks[%SelectedSkill.selected].active.damage)))
+	
+	GlobalValues.opponent[0] -= _attack_affinity(equippedAttacks[%SelectedSkill.selected].type, int(equippedAttacks[%SelectedSkill.selected].active.damage))
 	_on_turn_advanced()
+
+func _load_enemy():
+	var enemyDir = "res://JSON/Enemies/" + GlobalValues.opponent +".json"
+	var curFile_as_text = FileAccess.get_file_as_string(enemyDir)
+	var json = JSON.new()
+	var curFile_as_dict = json.parse(curFile_as_text)
+	if curFile_as_dict == OK:
+		var data = json.data
+		enemyMaxHealth = int(data.health)
+		$EnemyDetails/Name.text = data.name
+		$EnemyDetails/Health.text = "Health: " + str(int(data.health)) + "/" + str(enemyMaxHealth)
+		GlobalValues.opponent = [data.health] #0
+		GlobalValues.opponent.append(data.attacks)#1
+		GlobalValues.opponent.append(data.weaknesses)#2
+		GlobalValues.opponent.append(data.resistances)#3
+		GlobalValues.opponent.append(data.rewards)#4
+	else:
+		print("JSON Parse Error: ", json.get_error_message(), " in ", enemyDir, " at line ", json.get_error_line())
+		
+func _load_player_attacks():
+	var abilityDir = "res://JSON/Player_Abilites/"
+	for abilityFile in DirAccess.get_files_at(abilityDir):
+		var curFile = abilityDir + abilityFile
+		var curfile_as_text = FileAccess.get_file_as_string(curFile)
+		var json = JSON.new()
+		var curFile_as_dict = json.parse(curfile_as_text)
+		if curFile_as_dict == OK:
+			var data = json.data
+			for equipped in GlobalValues.equipped:
+				if equipped == data.name:
+					print(data.name)
+					if "active" in data:
+						%SelectedSkill.add_item(data.name)
+						equippedAttacks.append(data)
+		else:
+			print("JSON Parse Error: ", json.get_error_message(), " in ", abilityFile, " at line ", json.get_error_line())
